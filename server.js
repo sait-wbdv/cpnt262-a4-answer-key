a749751564a96de016038b82d5b5b01e4614f7c4
Tony Grimes <acidtone@tonygrimes.com>
2021-03-26 13:20:45 -0600

    1.2 set view engine

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.set('view engine', `ejs`)
 // runs first
 
 app.use(express.static(path.join(__dirname, './public')));
